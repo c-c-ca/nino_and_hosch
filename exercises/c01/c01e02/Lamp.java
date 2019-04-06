d023d52c023f5c6e3f1436d0715a0e4f9c6f370f
Adam Carter <adm.carter@gmail.com>
2019-04-06 15:37:34 -0400

    finish c01e02

---
+++ exercises/c01/c01e02/Lamp.java
@@ -0,0 +1,22 @@
+/**
+ * A simple three-way lamp.
+ */
+public class Lamp {
+
+    private int setting;
+
+    /**
+     * Create a new Lamp.
+     */
+    public Lamp () {}
+
+    /**
+     * The current setting.
+     */
+    public int currentSetting () {}
+
+    /**
+     * Change the current setting.
+     */
+    public void changeSetting () {}
+}
