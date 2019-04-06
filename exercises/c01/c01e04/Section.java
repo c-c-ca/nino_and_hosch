ab4f163040b972dc8bdb04b7ded2d4ece929967a
Adam Carter <adm.carter@gmail.com>
2019-04-06 16:02:35 -0400

    finish c01e03

---
+++ exercises/c01/c01e04/Section.java
@@ -0,0 +1,19 @@
+/**
+ * Models a class section in a university class scheduling system.
+ */
+class Section {
+
+    private int classSize;
+    private double time;
+    private Course course;
+    private FacultyMember instructor;
+    private ClassRoom room;
+    private Date startDate;
+    private Date endDate;
+
+    /**
+     * Create a new Section.
+     */
+    public Section () {}
+
+}
