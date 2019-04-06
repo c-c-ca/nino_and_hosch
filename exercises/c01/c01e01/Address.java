fb735495eea287f61f1a951aabe0cd1b31be7b07
Adam Carter <adm.carter@gmail.com>
2019-04-06 15:19:39 -0400

    add solution to c01e01

---
+++ exercises/c01/c01e01/Address.java
@@ -0,0 +1,12 @@
+/**
+ * Models a simple street address.
+ */
+public class Address {
+
+    private String houseNumber;
+    private String streetName;
+    private String city;
+    private String state;
+    private int zipCode;
+
+}
