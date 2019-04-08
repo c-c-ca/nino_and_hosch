5d924f3d0c82d968e39231036f47b5e3f52c8a18
Adam Carter <adm.carter@gmail.com>
2019-04-08 13:21:12 -0400

    finish c02e11

---
+++ exercises/c02/c02e11/counters/Counter.java
@@ -0,0 +1,52 @@
+/**
+ * Models a simple integer counter.
+ */
+public class Counter {
+
+    // Private components:
+
+    private int count;
+
+    // Constructor:
+
+    /**
+     * Create a new Counter, with the count initialized
+     * to 0.
+     */
+    public Counter () {
+        count = 0;
+    }
+
+    // Queries:
+
+    /**
+     * The number of items counted.
+     */
+    public int currentCount () {
+        return count;
+    }
+
+    // Commands:
+
+    /**
+     * Increment the count by 1.
+     */
+    public void incrementCount () {
+        count = count + 1;
+    }
+
+    /**
+     * Reset the count to 0.
+     */
+    public void reset () {
+        count = 0;
+    }
+
+    /**
+     * Doubles the count.
+     */
+    public void double () {
+        count *= 2;
+    }
+
+}
