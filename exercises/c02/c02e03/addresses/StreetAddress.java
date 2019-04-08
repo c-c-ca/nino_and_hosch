e9acf70ee7bd646c79f11e17e75de275db879721
Adam Carter <adm.carter@gmail.com>
2019-04-07 20:31:37 -0400

    finish c02e03

---
+++ exercises/c02/c02e03/addresses/StreetAddress.java
@@ -0,0 +1,42 @@
+package addresses;
+
+/**
+ * Models a street address.
+ */
+class StreetAddress {
+
+    // Constructor:
+
+    /**
+     * Create a new StreetAddress.
+     */
+    public StreetAddress (int number, String street, String city,
+                          String state, int zip) {}
+
+    // Queries:
+
+    /**
+     * The street address number.
+     */
+    public int number () { return 0; }
+
+    /**
+     * The street name.
+     */
+    public String street () { return null; }
+
+    /**
+     * The city name.
+     */
+    public String city () { return null; }
+
+    /**
+     * The state name.
+     */
+    public String state () { return null; }
+
+    /**
+     * The zip code.
+     */
+    public int zip () { return 0; }
+}
