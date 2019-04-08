69be28677455b53a836d76868673954e953a0d81
Adam Carter <adm.carter@gmail.com>
2019-04-08 18:11:56 -0400

    finish c02e15

---
+++ exercises/c02/c02e15/shapes/Test.java
@@ -0,0 +1,15 @@
+package shapes;
+
+/**
+ * Test the class Rectangle.
+ */
+public class Test {
+
+    /**
+     * Run a Rectangle test.
+     */
+    public static void main (String[] args) {
+        RectangleTester tester = new RectangleTester();
+        tester.start();
+    }
+}
