b5c97cc02c5a56b570897a4bfaff963724c3cd28
Adam Carter <adm.carter@gmail.com>
2019-04-10 16:03:10 -0400

    finish c03e02

---
+++ exercises/c03/c03e02/classSection/Student.java
@@ -0,0 +1,41 @@
+package classSection;
+
+/**
+ * Models a student in a class section.
+ */
+public class Student {
+
+    // Named constants:
+
+    /**
+     * The maximum score a student can receive.
+     */
+    public final static int MAX_SCORE = 100;
+
+
+    // Private components:
+
+    private String name;    // name
+    private int grade;      // current grade
+
+    // Constructors:
+
+    /**
+     * Create a new Student initialized with the specified
+     * name and grade.  grade must be a non-negative integer.
+     */
+    public Student (String name, int grade) {
+        this.name = name;
+        this.grade = grade;
+    }
+
+    // Queries:
+
+    /**
+     * The adjusted grade for this Student.
+     */
+    public int adjustedGrade (int highGrade) {
+        int maxAdjustment = MAX_SCORE - highGrade;
+        return grade + grade * maxAdjustment / highGrade;
+    }
+}
