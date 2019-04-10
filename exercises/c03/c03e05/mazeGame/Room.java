eb914b483591fbcb4997cc839ffad514cc674d87
Adam Carter <adm.carter@gmail.com>
2019-04-10 17:48:37 -0400

    add Room.java

---
+++ exercises/c03/c03e05/mazeGame/Room.java
@@ -0,0 +1,30 @@
+package mazeGame;
+
+/**
+ * A room in the maze.
+ */
+public class Room {
+
+  // Private components:
+
+  private String description;
+
+  // Constructors:
+
+  /**
+   * Create a new Room with the specified decription.
+   */
+  public Room (String description) {
+    this.description = description;
+  }
+
+  // Queries:
+
+  /**
+   * String representation of this Room.
+   */
+  public String toString () {
+    return "Room: " + description;
+  }
+
+}
