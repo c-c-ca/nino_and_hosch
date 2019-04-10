19f632e90561dc5543cf45a3e7f7d3e2b86ac19d
Adam Carter <adm.carter@gmail.com>
2019-04-10 17:55:06 -0400

    add Explorer.java to c03e05 and c03e06 solution

---
+++ exercises/c03/c03e06/mazeGame/Room.java
@@ -0,0 +1,55 @@
+package mazeGame;
+
+/**
+ * A room in the maze.
+ */
+public class Room {
+
+  // Private components:
+
+  private String description;
+  private Boolean occupied;    // an explorer is in the room
+
+  // Constructors:
+
+  /**
+   * Create a new Room with the specified decription.
+   */
+  public Room (String description) {
+    this.description = description;
+    this.occupied = false;
+  }
+
+  // Queries:
+
+  /**
+   * An Explorer is currently located in this Room.
+   */
+  public Boolean occupied () {
+      return occupied;
+  }
+
+  /**
+   * String representation of this Room.
+   */
+  public String toString () {
+    return "Room: " + description;
+  }
+
+  // Commands:
+
+  /**
+   * The Explorer has entered this Room.
+   */
+  public void enter () {
+      occupied = true;
+  }
+
+  /**
+   * The Explorer has exited this Room.
+   */
+  public void exit () {
+      occupied = false;
+  }
+
+}
