d978268bd7eae5ccec90d55d5a62d99e8d3abf5a
Adam Carter <adm.carter@gmail.com>
2019-04-12 15:26:35 -0400

    revise error messages in test class

--- exercises/c04/c04e12/counters/TestBaseballCounterClass.java
+++ exercises/c04/c04e12/counters/TestBaseballCounterClass.java
@@ -42,15 +42,15 @@ class BaseballCounterTest {
         counter.incrementBalls();  // test ball count reset after 3rd strike
         // strike 1
         counter.incrementStrikes();
-        verify(counter.strikes() == 1, "increment strike count, 0 strikes");
+        verify(counter.strikes() == 1, "increment strike count for counter with a strike count of 0");
         verify(counter.balls() == 1, "increment strike count, ball count");
         // strike 2
         counter.incrementStrikes();
-        verify(counter.strikes() == 2, "increment strike count, 1 strike");
+        verify(counter.strikes() == 2, "increment strike count for counter with a strike count of 1");
         verify(counter.balls() == 1, "increment strike count, ball count");
         // strike 3
         counter.incrementStrikes();
-        verify(counter.strikes() == 0, "increment strike count, 2 strikes");
+        verify(counter.strikes() == 0, "increment strike count for counter with a strike count of 2");
         verify(counter.balls() == 0, "increment strike count, ball count");
     }
 
@@ -62,19 +62,19 @@ class BaseballCounterTest {
         // ball 1
         counter.incrementBalls();
         verify(counter.strikes() == 1, "increment ball count, strike count");
-        verify(counter.balls() == 1, "increment ball count, 0 balls");
+        verify(counter.balls() == 1, "increment ball count for counter with a ball count of 0");
         // ball 2
         counter.incrementBalls();
         verify(counter.strikes() == 1, "increment ball count, strike count");
-        verify(counter.balls() == 2, "increment ball count, 1 ball");
+        verify(counter.balls() == 2, "increment ball count for counter with a ball count of 1");
         // ball 3
         counter.incrementBalls();
         verify(counter.strikes() == 1, "increment ball count, strike count");
-        verify(counter.balls() == 3, "increment ball count, 2 balls");
+        verify(counter.balls() == 3, "increment ball count for counter with a ball count of 2");
         // ball 4
         counter.incrementBalls();
         verify(counter.strikes() == 0, "increment ball count, strike count");
-        verify(counter.balls() == 0, "increment ball count, 3 balls");
+        verify(counter.balls() == 0, "increment ball count for counter with a ball count of 3");
     }
 
     /**
