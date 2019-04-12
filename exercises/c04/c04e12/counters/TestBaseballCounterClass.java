efc626a687819d226f5c80eab0de9ef942a8e594
Adam Carter <adm.carter@gmail.com>
2019-04-12 15:21:11 -0400

    finish c04e12

---
+++ exercises/c04/c04e12/counters/TestBaseballCounterClass.java
@@ -0,0 +1,101 @@
+package counters;
+
+/**
+ * A simple class to test the class BaseballCounter.
+ */
+class BaseballCounterTest {
+
+    // Private components:
+
+    private BaseballCounter counter;  // the object to test
+
+    // Constructors:
+
+    /**
+     * Create a new BaseballCounterTest.
+     */
+    public BaseballCounterTest () {
+        counter = new BaseballCounter();
+    }
+
+    /**
+     * Run the test.
+     */
+    public void runTest () {
+        testInitialState();
+        testIncrementStrikes();
+        testIncrementBalls();
+    }
+
+    /**
+     * Test the BaseballCounter's initial state.
+     */
+    private void testInitialState () {
+        verify(counter.strikes() == 0, "initial state, number of strikes");
+        verify(counter.balls() == 0, "initial state, number of balls");
+    }
+
+    /**
+     * Test the incrementStrikes method.
+     */
+    private void testIncrementStrikes () {
+        counter.incrementBalls();  // test ball count reset after 3rd strike
+        // strike 1
+        counter.incrementStrikes();
+        verify(counter.strikes() == 1, "increment strike count, 0 strikes");
+        verify(counter.balls() == 1, "increment strike count, ball count");
+        // strike 2
+        counter.incrementStrikes();
+        verify(counter.strikes() == 2, "increment strike count, 1 strike");
+        verify(counter.balls() == 1, "increment strike count, ball count");
+        // strike 3
+        counter.incrementStrikes();
+        verify(counter.strikes() == 0, "increment strike count, 2 strikes");
+        verify(counter.balls() == 0, "increment strike count, ball count");
+    }
+
+    /**
+     * Test the incrementBalls method.
+     */
+    private void testIncrementBalls () {
+        counter.incrementStrikes(); // test strike count reset after 4th ball
+        // ball 1
+        counter.incrementBalls();
+        verify(counter.strikes() == 1, "increment ball count, strike count");
+        verify(counter.balls() == 1, "increment ball count, 0 balls");
+        // ball 2
+        counter.incrementBalls();
+        verify(counter.strikes() == 1, "increment ball count, strike count");
+        verify(counter.balls() == 2, "increment ball count, 1 ball");
+        // ball 3
+        counter.incrementBalls();
+        verify(counter.strikes() == 1, "increment ball count, strike count");
+        verify(counter.balls() == 3, "increment ball count, 2 balls");
+        // ball 4
+        counter.incrementBalls();
+        verify(counter.strikes() == 0, "increment ball count, strike count");
+        verify(counter.balls() == 0, "increment ball count, 3 balls");
+    }
+
+    /**
+     * Verify the specified test condition.  Print message if the test fails.
+     */
+    private void verify (Boolean test, String message) {
+        if (!test)
+            System.out.println(
+                "Verification failed: " + message);
+    }
+} // end of class BaseballCounterTest
+
+/**
+ * A simple test system for the class BaseballCounter.
+ */
+public class TestBaseballCounterClass {
+
+    /**
+     * Run a DateTest.
+     */
+    public static void main (String[] args) {
+        (new BaseballCounterTest()).runTest();
+    }
+}
