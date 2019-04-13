a6339984b685ce4baf441cffbb36f3f59aa410a2
Adam Carter <adm.carter@gmail.com>
2019-04-13 17:22:04 -0400

    add assert statements to verify non-null references to name and location in constructor method for class Explorer

---
+++ exercises/c05/c05e05/mazeGame/PassageWay.java
@@ -0,0 +1,18 @@
+package mazeGame;
+
+/**
+ * A passageway in the maze.
+ */
+public class PassageWay {
+
+    /**
+     * The Room this PassageWay connects from.
+     */
+    public Room toRoom () { return null; }
+
+    /**
+     * The Room this PassageWay connects to.
+     */
+    public Room fromRoom () { return null; }
+
+}
