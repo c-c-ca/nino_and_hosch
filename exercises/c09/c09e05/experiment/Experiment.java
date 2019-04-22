4031af5c6629be128816281910eebbcefc810d96
Adam Carter <adm.carter@gmail.com>
2019-04-21 21:41:01 -0400

    experiment to find out if an interface can specify a static method

---
+++ exercises/c09/c09e05/experiment/Experiment.java
@@ -0,0 +1,9 @@
+package experiment;
+
+public class Experiment {
+    public static void main (String[] argv) {
+        ExperimentInterface i;
+        i = new ExperimentClass();
+        i.callMethod();
+    }
+}
---
+++ exercises/c09/c09e05/experiment/ExperimentClass.java
@@ -0,0 +1,8 @@
+package experiment;
+
+public class ExperimentClass implements ExperimentInterface {
+
+    public static void callMethod () {
+        System.out.println("Hello, world");
+    }
+}
