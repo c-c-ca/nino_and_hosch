828e87a4d1eac32c52bb5732aaab23bc77e08882
Adam Carter <adm.carter@gmail.com>
2019-04-21 20:56:52 -0400

    define classes Addition and Multiplication to implement the OperatorWithIdentity interface

---
+++ exercises/c09/c09e04/operations/OperatorWithIdentityTest.java
@@ -0,0 +1,53 @@
+package operations;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * Test class Addition and Multiplication that implement
+ * the OperatorWithIdentity interface.
+ */
+public class OperatorWithIdentityTest {
+
+    // Test fixture:
+
+    private Addition add;
+    private Multiplication mult;
+
+    @Before
+    public void setUp () {
+        add = new Addition();
+        mult = new Multiplication();
+    }
+
+    private void testIdentity (OperatorWithIdentity op,
+                          int i) {
+        assertEquals(
+            op.operate(op.identity(),i),
+            op.operate(i,op.identity()));
+        assertEquals(i,op.operate(i,op.identity()));
+        assertEquals(op.operate(i,op.identity()),i);
+    }
+
+    /**
+     * Test the class Addition.
+     */
+    @Test
+    public void testAdditionIdentity () {
+        testIdentity(add,0);
+        testIdentity(add,1);
+        testIdentity(add,10);
+    }
+
+    /**
+     * Test the class Addition.
+     */
+    @Test
+    public void testMultiplicationIdentity () {
+        testIdentity(mult,0);
+        testIdentity(mult,1);
+        testIdentity(mult,10);
+    }
+
+
+}
